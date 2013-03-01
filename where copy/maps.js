55b4f2dba4ad7c4391eaabe803254d296f16bd62
Neena Kapur <neena.kapur@tufts.edu>
2013-03-01 15:27:53 -0500

    testing copy--for display purposes

---
+++ where copy/maps.js
@@ -0,0 +1,36 @@
+function init(){
+		mapSetting={
+		center: new google.maps.LatLng(42.3000, -71.8000),
+		zoom: 8,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	};
+	
+ 	map = new google.maps.Map(document.getElementById("map_canvas"),
+            mapSetting);
+    myLocation;        
+}
+
+function myLocation(){
+	if(navigator.geolocation){
+		navigator.geolocation.getCurrentPosition(function(position){
+		lat=position.coords.latitude;
+		lng=position.coords.longitude;
+		console.log(longitude)
+		var location=new google.maps.LatLng(latitude, longitude);
+
+		mapSetting={
+			center:location,
+			mapTypeId: google.maps.MapTypeID.ROADMAP
+		};
+		
+		//placing marker
+		var mark_point=new google.maps.Marker({
+			position: location,
+			map: map,
+			title: "You are here"
+			});
+
+		});
+	
+	}
+}
