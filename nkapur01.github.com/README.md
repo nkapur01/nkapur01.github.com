96cfddc9fb17f0e6b275628db590b8899134d69a
Neena Kapur <neena.kapur@tufts.edu>
2013-03-01 16:23:19 -0500

    idk

---
+++ nkapur01.github.com/README.md
@@ -0,0 +1,16 @@
+nkapur01.github.com
+===================
+
+Assignment one: Personal Portfolio
+
+I have successfully implemented the following:
+index.html--contains my name, an image, and links to my resume and bio
+bio.html--contains a short bio, links to the home page and resume
+resume.html--contains my resume, links to the home page and bio
+
+I asked Will Clarkson for help in rendering my mobile stylesheet on a phone. I had difficulty implementing it at first.
+
+I am still having a little difficulty formatting my mobile stylesheet. It opens and is very readable on a mobile device, but I could definitely improve my formatting.
+
+I worked for 5-6  hours on this project. 
+
---
+++ nkapur01.github.com/wherecopy/maps.js
@@ -0,0 +1,37 @@
+function init(){
+		mapSetting={
+		center: new google.maps.LatLng(42.3000, -71.8000),
+		zoom: 8,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	};
+	
+ 	map = new google.maps.Map(document.getElementById("map_canvas"),
+            mapSetting);
+
+    myLocation();
+
+}
+
+function myLocation(){
+
+	if(navigator.geolocation){
+		navigator.geolocation.getCurrentPosition(function(position){
+		lat=position.coords.latitude;
+		lng=position.coords.longitude;
+		console.log(lng);
+		var location=new google.maps.LatLng(lat, lng);
+		console.log("made location");
+		map.setCenter(location);
+		console.log("set new center");
+		var marker=new google.maps.Marker({
+			
+			position: location,
+			map: map,
+			title: "You are here"
+			});
+			console.log("marker made!");
+
+		});
+	
+	}
+}
